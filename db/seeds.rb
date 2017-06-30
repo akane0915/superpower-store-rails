7fa94e8502b7882d5f2794d1cbf5f0f94f5dcb64
akane0915 <asialkane@gmail.com>
2017-06-30 13:52:14 -0700

    seed database with 250 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 Product.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Superhero.unique.power,
+  new_product = Product.create!(name: Faker::Superhero.unique.power,
                         price: Faker::Commerce.price,
                         country: Faker::Address.country)
+  5.times do
+    new_product.reviews.create!(author: Faker::Superhero.name, content: Faker::Lorem.characters(100), rating: Faker::Number.between(1, 5))
+  end
 end
 
 p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
